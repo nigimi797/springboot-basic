b24ecca72630eb698faa4c71f49fe5a5e47829b3
lcs1108 <lcs@ex-it.co.kr>
2022-10-27 16:15:07 +0900

    섹션 6. 컴포넌트 스캔 - 중복 등록과 충돌

---
+++ src/main/java/com/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package com.hello.core;
+
+import com.hello.core.member.MemberRepository;
+import com.hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        // basePackage : 탐색할 패키지의 시작 위치를 지정
+        // basePackages = "com.hello.core.member",
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+
+    // 수동 빈 등록시 수동 빈이 자동 빈을 오버라이딩 해버린다.
+    @Bean(name = "memoryMemberRepository")
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
