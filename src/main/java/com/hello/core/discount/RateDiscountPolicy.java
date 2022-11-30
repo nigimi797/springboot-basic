57eb19fc8f9eef6aacb5e196378bb4d37b1444de
lcs1108 <lcs@ex-it.co.kr>
2022-11-30 16:04:40 +0900

    섹션 8. 빈 생명주기 콜백 - 애노테이션 @PostConstruct, @PreDestroy

--- src/main/java/com/hello/core/discount/RateDiscountPolicy.java
+++ src/main/java/com/hello/core/discount/RateDiscountPolicy.java
@@ -1,13 +1,15 @@
 package com.hello.core.discount;
 
+import com.hello.core.annotation.MainDiscountPolicy;
 import com.hello.core.member.Grade;
 import com.hello.core.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+@MainDiscountPolicy
 public class RateDiscountPolicy implements DiscountPolicy {
 
-    private int discountPercent = 10;
+    private int discountPercent = 20;
 
     @Override
     public int discount(Member member, int price) {
